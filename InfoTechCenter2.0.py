7398941a0b6a3e202c72b3aa8bd4f4f7f1c3c91e
Charlotte Osburn <charlottejosburn@hotmail.com>
2022-02-22 13:29:17 -0500

    Imported Colorama - Added to Print Statements

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -1,7 +1,10 @@
 # Code Name - Hornet
 
 from time import sleep  #Print to one line with time delay between prints
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(strip=False, autoreset=True)
 
-print("Welcome to Hornets InfoTechCenter\n"); sleep(1)
+print(Fore.LIGHTMAGENTA_EX + Back.BLACK + Style.BRIGHT + "Welcome to Hornets InfoTechCenter\n"); sleep(1)
 
-print("Hornet's Operating System Booting Up")
+print(Fore.LIGHTCYAN_EX + Back.WHITE + Style.DIM + "Hornet's Operating System Booting Up")
