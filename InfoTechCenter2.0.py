de38b68f540fd3005227d8824b01d56709dc99bd
Charlotte Osburn <charlottejosburn@hotmail.com>
2022-02-24 13:55:28 -0500

    Declared vehicleResponseSystem function - IF - Stable

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -3,12 +3,17 @@ import random
 # Weather Branch
 
 def weather():
-    weatherForecast = ["Icey","Snowy","Raining","Windy","Foggy","Sunny"]
+    weatherForecast = ["Icy","Snowy","Raining","Windy","Foggy","Sunny"]
     weatherConditions = random.choice(weatherForecast)
     return weatherConditions
 
 # Calling weather Function to determine weather
 weatherAlert = weather()
 
-print(weatherAlert)
+def vehicleResponseSystem():
+    if weatherAlert == "Icy":
+        print("\nVRS has changed your Alarm 30 minutes earlier based on the NWS forecast of",weatherAlert,":(")
+        print("VRS will only allow your car to go 30MPH")
+
+vehicleResponseSystem()
 
